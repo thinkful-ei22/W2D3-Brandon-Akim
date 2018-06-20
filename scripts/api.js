fb90bd6826b287e1e50bdc83022192f1278d8d94
Brandon Graham <bgraham452@gmail.com>
2018-06-20 12:13:32 -0400

    Wrote the getItems() fn

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,14 @@
+'use strict';
+/* global $ */
 const api = (function(){
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[Brandon-Akim]'
-    const getItems = callback => callback('api module works!');
-    return {
-        getItems
-    }
-}())
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[Brandon-Akim]';
+  const getItems = callback => {
+  
+    // $.getJSON(BASE_URL+'/items', callback);
+  
+    callback('api module works!');
+  };
+  return {
+    getItems
+  };
+}());
